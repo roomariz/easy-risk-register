40ec4ef18959a68906c68be9ef71fba14e1ac7ad
roomariz <roomariz@gmail.com>
2025-11-09 02:33:53 +0100

    Add frontend application structure for Easy Risk Register

---
+++ easy-risk-register-frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Easy Risk Register</h1>
+      </div>
+      <h2>Welcome to the starter project</h2>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Get started by editing <code>src/App.tsx</code>
+      </p>
+    </>
+  )
+}
+
+export default App
